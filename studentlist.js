37d0c90bdf7867c6ae41dd2693a453397a70f589
bricemodeste <bricemodeste@Brices-MacBook-Pro.local>
2019-02-26 16:28:59 +0100

    properties js + data class html

--- studentlist.js
+++ studentlist.js
@@ -2,27 +2,24 @@
 
 const template = document.querySelector("#studentTemplate").content;
 const urlJson = "http://petlatkea.dk/2019/hogwarts/students.json";
-
+let arrayOfStudents = [];
+let filterArray;
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   console.log("init");
-fetch("http://petlatkea.dk/2019/hogwarts/students.json")
+  fetch("http://petlatkea.dk/2019/hogwarts/students.json")
     .then(res => res.json()) // Get the response as a JSON format
     .then(getJSON); // Call the next function
 }
 
 function getJSON(studentList) {
-  console.log("getJSON");
+  console.log("getJson");
 
-     // Execute the function on each element
+  // Execute the function on each element
   studentList.forEach(showSingleStudent);
-
-
 }
-
-
 
 // Declare the callback function to show single student
 function showSingleStudent(student) {
@@ -40,3 +37,76 @@ function showSingleStudent(student) {
 
 // Call the function
 init();
+
+const Hogward_Student = {
+  // - add properties and values
+  firstName: "-firstName-",
+  lastName: "-lastName-",
+  house: "-house-",
+  // - function for divided fullname
+  splitingName(fullName) {
+    const firstSpace = fullName.indexOf(" ");
+    const lastSpace = fullName.lastIndexOf(" ");
+    this.firstName = fullName.slice(0, firstSpace + 1);
+    this.lastName = fullName.slice(lastSpace + 1);
+  },
+  assignHouse(student) {
+    this.house = student.house;
+  }
+};
+function getnameLength(firstSpace) {
+  return firstSpace.length;
+  console.log(firstSpace("john"));
+}
+
+function buildObjects(dataList) {
+  dataList.forEach(onedata => {
+    let student = Object.create(Hogward_Student);
+    student.splitingName(onedata.fullname);
+    student.assignHouse(onedata);
+    // console.log(student);
+    arrayOfStudents.push(student);
+  });
+  // console.log(arrayOfStudents);
+  filterArray = arrayOfStudents;
+  filterByHouse(filterArray);
+}
+
+function filterByHouse(filterArray) {
+  filterArray.filter(onlyGryffindor);
+  filterArray.filter(onlyHufflepuff);
+  filterArray.filter(onlyRavenclaw);
+  filterArray.filter(onlySlytherin);
+}
+
+function onlyGryffindor(element) {
+  if (element.house === "Gryffindor") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function onlyHufflepuff(element) {
+  if (element.house === "Hufflepuff") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function onlyRavenclaw(element) {
+  if (element.house === "Ravenclaw") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function onlySlytherin(element) {
+  if (element.house === "Slytherin") {
+    return true;
+  } else {
+    return false;
+  }
+}
